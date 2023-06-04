41b55d8b0d05ac771c6b0052bdea77f011b31f64
medranow <waldro4513@gmail.com>
2023-06-03 18:46:52 -0600

    Began working on my models and created a superuser

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import User, Post
+
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "username")
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Post)
+
--- network/models.py
+++ network/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.id}"
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="poster")
+    post = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.id}: a post from {self.user}. Test: {self.post}"
