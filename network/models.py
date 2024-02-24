87851472f2e025d8a1c337f76db5b3eed7e0f754
medranow <waldro4513@gmail.com>
2024-02-24 10:35:09 -0600

    I added a field like in my Post model)

--- network/models.py
+++ network/models.py
@@ -12,6 +12,7 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="poster")
     post = models.CharField(max_length=64)
     date = models.DateField()
+    like = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.id}: a post from {self.user}. Text: {self.post} on {self.date.strftime('%d %b %Y %H:%M:%S')}"
